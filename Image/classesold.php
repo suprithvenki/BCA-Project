fa983432a2d05ee46491321c9100d252a93326b9
suprithvenki <124327981+suprithvenki@users.noreply.github.com>
2024-03-01 12:38:29 +0530

    Add files via upload

---
+++ Image/classesold.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en" >
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,300&family=Poppins:wght@300&family=Raleway:wght@300&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,300&family=Raleway:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="classesold.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,300&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
+  <title>FitnessPortal</title>
+  </head>
+  <body>
+    <nav>
+    <div class="menu-bar">
+      <h1 class="logo">Fitness<span>Portal</span></h1>
+      <ul>
+        ....
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#fitness">FitnessCheck</a></li>
+        <li><a href="#contact">Contactus</a></li>
+        <li><a href="#login">Login</a></li>
+
+
+
+        <li><a href="#">Pages <i class="fas fa-caret-down"></i></a>
+            <div class="dropdown-menu">
+                <ul>
+                  <li><a href="#price">Pricing</a></li>
+                  <li><a href="#diet">Nutrition</a></li>
+                  <li>
+                    <a href="#blogs">Blog <i class="fas fa-caret-right"></i></a>
+                    
+                    <div class="dropdown-menu-1">
+                      <ul>
+                        <li><a href="#blog">Blog-1</a></li>
+                        <li><a href="#blog2">Blog-2</a></li>
+                        <li><a href="#blog3">Blog-3</a></li>
+                        <li><a href="#blog4">Blog-4</a></li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li><a href="#trainers">Trainers</a></li>
+                  <li><a href="#faq">FAQ</a></li>
+
+                </ul>
+              </div>
+        </li>
+        
+    </div>
+    <div class="hero">
+      &nbsp;
+    
+    
+   
+</nav>
+
+    <div class="h1">
+      <!-- Slider main container -->
+<div class="swiper">
+  <!-- Additional required wrapper -->
+  <div class="swiper-wrapper">
+    <!-- Slides -->
+    <div class="swiper-slide"><img src="E1.jpg"  width="200" height="600" alt="Welcome"></div>
+    <div class="swiper-slide"><img src="hey.jpg" width="200" height="600" alt="Welcome" ></div>
+    <div class="swiper-slide"><img src="H1.jpg" width="200" height="600" alt="Welcome" ></div>
+  
+  </div>
+  <!-- If we need pagination -->
+  <div class="swiper-pagination"></div>
+
+  <!-- If we need navigation buttons -->
+  <div class="swiper-button-prev"></div>
+  <div class="swiper-button-next"></div>
+</div>
+    </div>
+ 
+    <div id="about" class="card">
+      <section class="my-7">
+        <div class="py-4">
+        <h2 class="text-center">About US</h2></div>
+        <div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-6 col-md-6 col-12">
+      <img src="ab.jpg" width="75%" height="75%" class="img-fluid aboutimg">
+  </div>
+  <div class="col-lg-6 col-md-6 col-12">
+    <b>
+      <h6 class="display-5"> STAY FIT</h6>
+      <p class="py-3">We Will Offer free services to people who wish to be fit</p>
+      <a href="about1.php" class="btn btn-danger">read more</a>
+  </div>
+   </div>
+</div>
+</section>
+</div>
+</div>
+
+<div id="services" class="card">
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">Services</h2></div>
+<div class="container-fluid">
+<div class="row">
+  <div class="col-lg-4 col-md-4 col-12">
+          <div class="card" >
+          <img src="yoga.jpg" class="card-img-top" alt="HELLO">
+          <div class="card-body">
+            <h5 class="card-title">Yoga</h5>
+            <p class="card-text">Our trained proffesionals will train you yoga</p>
+            <a href="yoga.html" class="btn btn-danger">Read more</a>
+          </div>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-4 col-12">
+          <div class="card" >
+          <img src="crossfit.jpg" class="card-img-top" alt="HELLO">
+          <div class="card-body">
+            <h5 class="card-title">CrossFit</h5>
+            <p class="card-text">Our trained proffesionals will train you crossfit</p>
+            <a href="crossfit.html" class="btn btn-danger">Read more</a>
+          </div>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-4 col-12">
+          <div class="card" >
+          <img src="zumba1.jpg" class="card-img-top" alt="HELLO">
+          <div class="card-body">
+            <h5 class="card-title">zumba</h5>
+            <p class="card-text">Our trained proffesionals will train you zumba</p>
+            <a href="zumba.html" class="btn btn-danger">Read more</a>
+          </div>
+</div>
+</div>
+
+</div>
+</div>
+</section>
+</div>
+
+
+<div id="fitness" class="card">
+      <section class="my-7">
+        <div class="py-4">
+        <h2 class="text-center">Fitness Check</h2></div>
+        <div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-6 col-md-6 col-12">
+      <img src="fit1.jpg" width="75%" height="75%" class="img-fluid aboutimg">
+  </div>
+  <div class="col-lg-6 col-md-6 col-12">
+    <b>
+      <h4 class="display-6"> BEFIT</h4>
+      <p class="py-3">We Will Offer free services to people who wish to be fit</p>
+      <a href="Befit.php" class="btn btn-danger">read more</a>
+  </div>
+   </div>
+</div>
+</section>
+</div>
+</div>
+
+<div id="contact" class="card">
+      <section class="my-5">
+        <div class="py-5">
+        <h2 class="text-center">Contactus</h2></div>
+        <div class="w-50 m-auto">
+          <form action="userinfo.php" method="post">
+            <div class="form-group">
+              <label> Username</label>
+              <input type="text" name="user" autocomplete="off" class="form-control">
+            </div>  
+            <div class="form-group">
+              <label> Email Id</label>
+              <input type="text" name="email" autocomplete="off" class="form-control">
+            </div>    
+            <div class="form-group">
+              <label> Mobile</label>
+              <input type="text" name="mobile" autocomplete="off" class="form-control">
+            </div>    
+            <div class="form-group">
+              <label> Comments</label>
+              <textarea class="form-control" name="comments"></textarea>
+            </div>  
+            <button type="submit" class="btn btn-danger">Submit</button>       
+          </form>
+        </div>
+      </section>
+      <footer>
+        <p class="p-3 bg-dark text-white text-center">@FitnessPortal</p>
+      </footer>
+
+   
+    
+    
+
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>  
+    <script>
+      const swiper = new Swiper('.swiper', {autoplay:{
+        delay:3000,
+        disableOnInteraction:false,
+      },
+  // Optional parameters/
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+ 
+});
+    </script>
+  </body>
+</html>
+
